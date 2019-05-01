90718d912d3a2c9741d1e6c00975bc3d529459c7
Harald <liljaharald@gmail.com>
2019-05-01 15:12:30 +0200

    Cleaned up

--- main.cpp
+++ main.cpp
@@ -16,14 +16,21 @@
 #include <arpa/inet.h>
 #include <unistd.h>
 #include <sys/time.h>
+#include <signal.h>
 #include <string.h>
 
 #define RECEIVE_PORT 9888
 #define SEND_PORT 9887
 #define HOST "127.0.0.1"
 
+#define MIN_SCANS 100
+#define SCAN_TIMEOUT_MS 10000
+
 // Threads
 pthread_t match;
+
+// Network
+int sockfd;
 
 //void *cox_linefit(void *param)(
 //
@@ -37,6 +44,7 @@ pthread_t match;
 //);
 
 using Eigen::MatrixXd;
+
 
 int start_lidar(){
 	struct sockaddr_in serv_addr;
@@ -112,95 +120,105 @@ int stop_lidar(){
 	return EXIT_SUCCESS;
 }
 
-int laser_scan(int scan_duration_ms){
+int start_lidar_server(){
 	struct sockaddr_in serv_addr, cli_addr;
 	socklen_t clilen;
-	struct timeval tp;
-	int valread, sockfd, newsockfd, number_of_ch, number_of_data;
-	char buffer[1024] = {0};
-	char header[5];
-	char data[4096];
-	int angle, quality, distance;
+	int sockfd, newsockfd;
 
 	if ((sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0)
 	{
 		perror("socket failed");
-		return -1;
+		exit(EXIT_FAILURE);
 	}
 
-    bzero((char *) &serv_addr, sizeof(serv_addr));
+	bzero((char *) &serv_addr, sizeof(serv_addr));
 	serv_addr.sin_family = AF_INET;
 	serv_addr.sin_addr.s_addr = INADDR_ANY;
 	serv_addr.sin_port = htons(RECEIVE_PORT);
 
-    if (bind(sockfd, (struct sockaddr *)&serv_addr,
-                                 sizeof(serv_addr))<0)
-    {
-        perror("bind failed");
-        exit(EXIT_FAILURE);
-    }
-    printf("bind success\n");
-    if (listen(sockfd, 5) < 0)
+	if (bind(sockfd, (struct sockaddr *)&serv_addr,
+								 sizeof(serv_addr))<0)
+	{
+		perror("bind failed");
+		exit(EXIT_FAILURE);
+	}
+	printf("bind success\n");
+	if (listen(sockfd, 5) < 0)
 	{
 		perror("listen failed");
 		exit(EXIT_FAILURE);
 	}
-    printf("listen success\n");
-    clilen = sizeof(cli_addr);
-    if ((newsockfd = accept(sockfd, (struct sockaddr *)&cli_addr, &clilen))<0)
+	printf("listen success\n");
+	clilen = sizeof(cli_addr);
+	if ((newsockfd = accept(sockfd, (struct sockaddr *)&cli_addr, &clilen))<0)
 	{
 		perror("rejected");
 		exit(EXIT_FAILURE);
 	}
-    printf("accepted\n");
+	printf("accepted\n");
+	close(sockfd);
+	return newsockfd;
+}
+
+void interrupt_handler(int s){
+	stop_lidar();
+	close(sockfd);
+	exit(1);
+}
+
+Eigen::ArrayXXf get_scan(int sockfd, int scan_duration_ms, int min_scans){
+	Eigen::ArrayXXf all_scans(min_scans, 3);
+	Eigen::ArrayXf one_scan(3);
+	struct timeval tp;
+	char header[5];
+	char data[4096];
+	int angle, quality, distance, number_of_ch, number_of_data;
+	int number_of_scans = 0;
+
 	gettimeofday(&tp, NULL);
 	long int start_time_ms = tp.tv_sec * 1000 + tp.tv_usec / 1000;
 	long int current_time_ms = start_time_ms;
-	while(current_time_ms < (start_time_ms + scan_duration_ms)){
+	while(current_time_ms < (start_time_ms + scan_duration_ms) && number_of_scans < min_scans){
 		bzero(header, 5);
-		number_of_ch = read(newsockfd, header, 5);
+		number_of_ch = read(sockfd, header, 5);
 		if(number_of_ch != 5){
 			break;
 		}
 		if((int)header[0] == 165){
 			number_of_data = (((int)header[2]<<16) + ((int)header[3]<<8) + ((int)header[4]));
-			read(newsockfd, data, number_of_data);
+			read(sockfd, data, number_of_data);
 			quality = (int)data[0]>>2;
 			angle = ((int)data[1]>>1) + ((int)data[2]>>8);
 			distance = ((int)data[3]) + ((int)data[4]>>8);
-			printf("%d, %d, %d\n", quality, angle, distance);
+			one_scan << quality, angle, distance;
+			all_scans.row(number_of_scans) = one_scan;
+			number_of_scans++;
 		}
 		gettimeofday(&tp, NULL);
 		current_time_ms = tp.tv_sec * 1000 + tp.tv_usec / 1000;
 	}
-    close(newsockfd);
-	close(sockfd);
-	return EXIT_SUCCESS;
+	return all_scans;
 }
 
 int main(){
+	// Interrupt handling to make sure lidar stops on SIGINT
+	struct sigaction sigIntHandler;
+	sigIntHandler.sa_handler = interrupt_handler;
+	sigemptyset(&sigIntHandler.sa_mask);
+    sigIntHandler.sa_flags = 0;
+    sigaction(SIGINT, &sigIntHandler, NULL);
 
-	int scan_duration = 10000;
+	Eigen::ArrayXXf last_scan(MIN_SCANS, 3);
 
 	start_lidar();
-	laser_scan(scan_duration);
-	stop_lidar();
-
-	Eigen::ArrayXXf map(4, 4);
-	Eigen::ArrayXf v(4);
-	v << 0, 0, 0, 310;
-	map.row(0) = v;
-	v << 0,310,260,310;
-	map.row(1) = v;
-	v << 260,310,260,0;
-	map.row(2) = v;
-	v << 260,0,0,0;
-	map.row(3) = v;
-	std::cout << map;
-
+	sockfd = start_lidar_server();
+	usleep(2000000);
+	while(1){
+		last_scan = get_scan(sockfd ,SCAN_TIMEOUT_MS, MIN_SCANS);
+		std::cout << last_scan << "\n";
+		usleep(2000000);
+	}
 	return 0;
-
-
 }
 
 
